77315c8acdb68ea21aca865d53f43d0ce8d8cd93
Daniel <danoladesign@gmail.com>
2024-08-21 16:13:09 -0400

    port002: setup up types / setup prisma schema

---
+++ types/project.ts
@@ -0,0 +1,11 @@
+export interface Project {
+    id: string;
+    slug: string;
+    title: string;
+    summary: string;
+    info: string;
+    link: string;
+    repo: string;
+    media: string[];
+    date: Date;
+}
