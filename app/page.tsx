6519478d43456743ebe36212e7283266513bf94f
Daniel <danoladesign@gmail.com>
2024-08-21 17:08:35 -0400

    port002: completed setup of db

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
+import {Project} from '@/types/project'
+import {getProjects} from '@/lib/projects'
 
-export default function Home() {
+export default async function Home() {
+  const projects: Project[] = await getProjects();
   return (
     <div>
       hello
+
+      {projects.map(proj => (
+        <img src={proj.media[0]}/>
+      ))}
     </div>
   );
 }
